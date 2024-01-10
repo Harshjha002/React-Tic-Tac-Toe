4d21707caaa8ef93fb6f677e2197878feb43fa1a
Harshjha002 <harshjha8453@gmail.com>
2024-01-10 11:57:13 +0530

    compeleted player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,19 @@
+import React from "react";
+import Player from "./components/Player";
 
-function App() {
-  
-
+const App = () => {
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
-}
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName="Player 1" symbol="X" />
+          <Player initialName="Player 2" symbol="O" />
+        </ol>
+        gameBoard
+      </div>
+      Log
+    </main>
+  );
+};
 
-export default App
+export default App;
